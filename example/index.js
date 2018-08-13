3dfcbee1be828859016f433ee270057033d7b211
潘何增 <panhezeng@xuetangx.com>
2018-08-13 10:47:59 +0800

    更新

--- example/index.js
+++ example/index.js
@@ -2,25 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import utils from '../dist/utils-pan.js'
-import { checkUpload } from '../dist/utils-pan.js'
+//import { checkUpload } from '../dist/utils-pan.js'
 
-// import checkUpload from '../dist/check-upload.js'
+import checkUpload from '../dist/check-upload.js'
+import textOverflow from '../dist/text-overflow'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {file: '上传检查结果展示'}
-    this.handleChange = this.handleChange.bind(this)
+    this.state = {checkUpload: '上传检查结果展示', textOverflow: '超过3个字省略'}
+    this.checkUploadHandle = this.checkUploadHandle.bind(this)
+    this.textOverflowHandle = this.textOverflowHandle.bind(this)
     this.fileInput = React.createRef()
   }
 
-  handleChange (event) {
+  checkUploadHandle (event) {
     const file = this.fileInput.current.files[0]
     if (file) {
-      this.setState({file: JSON.stringify(checkUpload(file, file.type, 10))})
+      this.setState({checkUpload: JSON.stringify(checkUpload(file, file.type, 10))})
     } else {
-      this.setState({file: ''})
+      this.setState({checkUpload: ''})
     }
+  }
+
+  textOverflowHandle (event) {
+    this.setState({textOverflow: textOverflow(this.state.textOverflow, 3)})
   }
 
   render () {
@@ -29,10 +35,14 @@ class App extends React.Component {
         <form>
           <label>
             Upload file:
-            <input type="file" ref={this.fileInput} onChange={this.handleChange}/>
+            <input type="file" ref={this.fileInput} onChange={this.checkUploadHandle}/>
           </label>
         </form>
-        <div>file: {this.state.file}</div>
+        <div>checkUpload: {this.state.checkUpload}</div>
+        <button onClick={this.textOverflowHandle}>
+          超过3个加省略符号[0p98765432qc
+        </button>
+        <div>textOverflow: {this.state.textOverflow}</div>
       </div>
     )
   }
