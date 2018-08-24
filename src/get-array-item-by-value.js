e00c6e2dbfd1041b7a4528a4f808ed935a44a261
Pan Hezeng <panhezeng@gmail.com>
2018-08-24 13:52:36 +0800

    增加getArrayItemByValue

--- src/get-array-item-by-value.js
+++ src/get-array-item-by-value.js
@@ -1,17 +1,14 @@
 /**
  * @param array 需要遍历的数组
- * @param value 删除与value相等的项
+ * @param value 返回与value相等的项
  * @param key 如果有key，则表示数组项是对象，则取对象此key的值和value比较
  */
 module.exports = function (array, value, key) {
-  const delIndex = array.findIndex(function (element) {
+  return array.find(function (element) {
     if (key) {
       return (element[key] === value)
     } else {
       return (element === value)
     }
   })
-  if (delIndex !== -1) {
-    array.splice(delIndex, 1)
-  }
 }
--- src/utils-pan.js
+++ src/utils-pan.js
@@ -2,6 +2,7 @@ module.exports = {
   checkUpload: require('./check-upload.js'),
   textOverflow: require('./text-overflow.js'),
   delArrayItemByValue: require('./del-array-item-by-value.js'),
+  getArrayItemByValue: require('./get-array-item-by-value.js'),
   cookies: require('./cookies.js'),
   storage: require('./storage.js'),
   xss: require('./xss.js')
