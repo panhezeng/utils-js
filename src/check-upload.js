8b8c79e743d5bd80a8bc3e003ef8e6c44f4ea10c
Pan Hezeng <panhezeng@gmail.com>
2018-08-29 16:38:44 +0800

    上传检查方法改成默认只检查file参数

--- src/check-upload.js
+++ src/check-upload.js
@@ -5,49 +5,56 @@
  * @param size 单位KB
  * @return {{validate: boolean, message: string, error:number}}
  */
-module.exports = function (file, type = 'image', size = 2048) {
-  if (Object.prototype.toString.call(file) !== '[object File]') {
-    throw new Error('file参数必须为File数据类型')
-  }
-
-  let isType = true
-  let typeName = file.type
-  if (type) {
-    isType = new RegExp(`^${type}`).test(file.type)
-    switch (type) {
-      case 'image':
-        typeName = '图片'
-        break
-      case 'audio':
-        typeName = '音频'
-        if (size === 2048) {
-          size = 51200
-        }
-        break
-      case 'video':
-        typeName = '视频'
-        if (size === 2048) {
-          size = 512000
-        }
-        break
-    }
-  }
-
-  const isLtSize = file.size / 1024 < size
+module.exports = function (file, type = '', size = -1) {
   const result = {
     message: '',
     error: 0,
     validate: true
   }
-  if (!isType) {
-    result.message = `只能上传${typeName}!`
-    result.error = 1
-    result.validate = false
-  }
-  if (!isLtSize) {
-    let error = size >= 1024 ? `${size / 1024}M` : `${size}KB`
-    result.message = `上传${typeName}大小不能超过${error}!`
-    result.error = 2
+  if (Object.prototype.toString.call(file) === '[object File]') {
+    let isType = true
+    let typeName = file.type
+    if (type) {
+      isType = new RegExp(`^${type}`).test(file.type)
+      switch (type) {
+        case 'image':
+          typeName = '图片'
+          if (size === -1) {
+            size = 2048
+          }
+          break
+        case 'audio':
+          typeName = '音频'
+          if (size === -1) {
+            size = 51200
+          }
+          break
+        case 'video':
+          typeName = '视频'
+          if (size === -1) {
+            size = 512000
+          }
+          break
+      }
+    }
+    let isLtSize = true
+    if (size > 0) {
+      isLtSize = file.size / 1024 < size
+    }
+    if (!isType) {
+      result.message = `只能上传${typeName}!`
+      result.error = 1
+      result.validate = false
+    }
+    if (!isLtSize) {
+      let error = size >= 1024 ? `${size / 1024}M` : `${size}KB`
+      result.message = `上传${typeName}大小不能超过${error}!`
+      result.error = 2
+      result.validate = false
+    }
+  } else {
+    result.message = 'file参数必须为File数据类型'
+    result.error = 3
     result.validate = false
   }
   return result
