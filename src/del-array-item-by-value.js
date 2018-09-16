9d34433fc884cd0b449e686dd449f13be569e9b3
Pan Hezeng <panhezeng@gmail.com>
2018-09-16 20:19:34 +0800

    更新注释

--- src/del-array-item-by-value.js
+++ src/del-array-item-by-value.js
@@ -1,7 +1,7 @@
 /**
  * @param array 需要遍历的数组
  * @param value 删除与value相等的项
- * @param key 如果有key，则表示数组项是对象，则取对象此key的值和value比较
+ * @param key 如果有key，则表示数组项是对象，则取该对象命名为此key的属性值和value比较
  */
 module.exports = function delArrayItemByValue (array, value, key) {
   const delIndex = array.findIndex(function (element) {
--- src/get-array-item-by-value.js
+++ src/get-array-item-by-value.js
@@ -1,7 +1,7 @@
 /**
  * @param array 需要遍历的数组
  * @param value 返回与value相等的项
- * @param key 如果有key，则表示数组项是对象，则取对象此key的值和value比较
+ * @param key 如果有key，则表示数组项是对象，则取该对象命名为此key的属性值和value比较
  */
 module.exports = function getArrayItemByValue (array, value, key) {
   return array.find(function (element) {
