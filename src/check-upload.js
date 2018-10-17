8f186ee9314b18f0d6adbbb2a54276e2accfc34a
Pan Hezeng <panhezeng@gmail.com>
2018-10-17 11:11:38 +0800

    size默认改为undefined

--- src/check-upload.js
+++ src/check-upload.js
@@ -2,10 +2,10 @@
  * 检查上传文件时是否满足要求
  * @param file
  * @param type 文件类型
- * @param size 单位KB
+ * @param size 单位KB，默认undefined，不限制大小
  * @return {{validate: boolean, message: string, error:number}}
  */
-module.exports = function checkUpload (file, type = '', size = -1) {
+module.exports = function checkUpload (file, type = '', size = undefined) {
   const result = {
     message: '',
     error: 0,
@@ -16,29 +16,30 @@ module.exports = function checkUpload (file, type = '', size = -1) {
     let typeName = file.type
     if (file.type && type) {
       isType = type.split(',').some(value => value && new RegExp(`^${value}`).test(file.type))
-      switch (file.type.split('/')[0]) {
+      const fileType = file.type.split('/')[0]
+      switch (fileType) {
         case 'image':
           typeName = '图片'
-          if (size === -1) {
+          if (size === undefined) {
             size = 2048
           }
           break
         case 'audio':
           typeName = '音频'
-          if (size === -1) {
+          if (size === undefined) {
             size = 51200
           }
           break
         case 'video':
           typeName = '视频'
-          if (size === -1) {
+          if (size === undefined) {
             size = 512000
           }
           break
       }
     }
     let isLtSize = true
-    if (size > 0) {
+    if (size) {
       isLtSize = file.size / 1024 < size
     }
     if (!isType) {
