e8da75b3c707c8378340e3bd3b3e3551509fa0f4
Pan Hezeng <panhezeng@gmail.com>
2019-03-12 17:48:40 +0800

    增加moment.durationFormatHMS方法

--- example/index.js
+++ example/index.js
@@ -1,60 +1,73 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 // import utils from '../dist/utils-pan.js'
 //import { checkUpload } from '../dist/utils-pan.js'
 
-import checkUpload from '../dist/check-upload.js'
-import textOverflow from '../dist/text-overflow.js'
-import getObjectItemByPath from '../dist/get-object-item-by-path.js'
-import moment from '../dist/moment.js'
-import xss from '../dist/xss.js'
+import checkUpload from "../dist/check-upload.js";
+import textOverflow from "../dist/text-overflow.js";
+import getObjectItemByPath from "../dist/get-object-item-by-path.js";
+import moment from "../dist/moment.js";
+import xss from "../dist/xss.js";
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {checkUpload: '上传检查结果展示', textOverflow: '超过3个字省略', object: {path: 'item'}}
-    this.checkUploadHandle = this.checkUploadHandle.bind(this)
-    this.textOverflowHandle = this.textOverflowHandle.bind(this)
-    this.fileInput = React.createRef()
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkUpload: "上传检查结果展示",
+      textOverflow: "超过3个字省略",
+      object: { path: "item" }
+    };
+    this.checkUploadHandle = this.checkUploadHandle.bind(this);
+    this.textOverflowHandle = this.textOverflowHandle.bind(this);
+    this.fileInput = React.createRef();
   }
 
-  checkUploadHandle (event) {
-    const file = this.fileInput.current.files[0]
+  checkUploadHandle(event) {
+    const file = this.fileInput.current.files[0];
     if (file) {
-      this.setState({checkUpload: JSON.stringify(checkUpload(file, file.type, 1))})
+      this.setState({
+        checkUpload: JSON.stringify(checkUpload(file, file.type, 1))
+      });
     } else {
-      this.setState({checkUpload: ''})
+      this.setState({ checkUpload: "" });
     }
   }
 
-  textOverflowHandle (event) {
-    this.setState({textOverflow: textOverflow(this.state.textOverflow, 3)})
+  textOverflowHandle(event) {
+    this.setState({ textOverflow: textOverflow(this.state.textOverflow, 3) });
   }
 
-  render () {
+  render() {
     return (
       <div>
         <form>
           <label>
             Upload file:
-            <input type="file" ref={this.fileInput} onChange={this.checkUploadHandle}/>
+            <input
+              type="file"
+              ref={this.fileInput}
+              onChange={this.checkUploadHandle}
+            />
           </label>
         </form>
         <div>checkUpload: {this.state.checkUpload}</div>
-        <button onClick={this.textOverflowHandle}>
-          超过3个字加省略符号
-        </button>
+        <button onClick={this.textOverflowHandle}>超过3个字加省略符号</button>
         <div>textOverflow: {this.state.textOverflow}</div>
-        <div>getObjectItemByPath: {getObjectItemByPath(this.state, 'object.path')}</div>
+        <div>
+          getObjectItemByPath: {getObjectItemByPath(this.state, "object.path")}
+        </div>
         <div>moment {moment().format("YYYY-MM-DD HH:mm:ss")}</div>
-        <div>xss {xss.cleanHtml("<strong>hello</strong><script>alert(/xss/);</script>end")}</div>
+        <div>moment durationFormatHMS {moment.durationFormatHMS(3601)}</div>
+        <div>
+          xss{" "}
+          {xss.cleanHtml(
+            "<strong>hello</strong><script>alert(/xss/);</script>end"
+          )}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
--- src/moment.js
+++ src/moment.js
@@ -1,83 +1,122 @@
-const moment = require('moment');
+const moment = require("moment");
 
-moment.calendarFormat = function (myMoment, now) {
-  let diff = myMoment.diff(now, 'days', true)
+moment.calendarFormat = function(myMoment, now) {
+  let diff = myMoment.diff(now, "days", true);
   if (myMoment.year() < now.year()) {
-    return 'sameElse'
+    return "sameElse";
   }
   if (diff < -now.weekday()) {
-    return 'lastMonth'
+    return "lastMonth";
   }
   if (diff < -1) {
-    return 'lastWeek'
+    return "lastWeek";
   }
   if (diff < 0) {
-    return 'lastDay'
+    return "lastDay";
   }
   if (diff < 1) {
-    return 'sameDay'
+    return "sameDay";
   }
-  return 'sameElse'
-}
+  return "sameElse";
+};
 
-moment.defineLocale('zh-cn-custom', {
-  parentLocale: 'zh-cn',
-  weekdaysShort: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+moment.defineLocale("zh-cn-custom", {
+  parentLocale: "zh-cn",
+  weekdaysShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
   calendar: {
-    lastDay: '[昨天]',
-    sameDay: 'A h:mm',
-    lastWeek: 'ddd',
-    lastMonth: 'l',
-    sameElse: 'L'
+    lastDay: "[昨天]",
+    sameDay: "A h:mm",
+    lastWeek: "ddd",
+    lastMonth: "l",
+    sameElse: "L"
   },
-  meridiem: function (hour, minute, isLowercase) {
+  meridiem: function(hour, minute, isLowercase) {
     if (hour < 9) {
-      return '早上'
+      return "早上";
     } else if (hour < 11 && minute < 30) {
-      return '上午'
+      return "上午";
     } else if (hour < 13 && minute < 30) {
-      return '中午'
+      return "中午";
     } else if (hour < 18) {
-      return '下午'
+      return "下午";
     } else {
-      return '晚上'
+      return "晚上";
     }
   }
-})
+});
 
-moment.defineLocale('en-custom', {
-  parentLocale: 'en',
-  weekdaysShort: ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'],
+moment.defineLocale("en-custom", {
+  parentLocale: "en",
+  weekdaysShort: ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."],
   calendar: {
-    lastDay: '[Yesterday]',
-    sameDay: 'A h:mm',
-    lastWeek: 'ddd',
-    lastMonth: 'l',
-    sameElse: 'L'
+    lastDay: "[Yesterday]",
+    sameDay: "A h:mm",
+    lastWeek: "ddd",
+    lastMonth: "l",
+    sameElse: "L"
   },
-  meridiem: function (hour, minute, isLowercase) {
+  meridiem: function(hour, minute, isLowercase) {
     if (hour < 9) {
-      return 'morning'
+      return "morning";
     } else if (hour < 11 && minute < 30) {
-      return 'morning'
+      return "morning";
     } else if (hour < 13 && minute < 30) {
-      return 'noon'
+      return "noon";
     } else if (hour < 18) {
-      return 'afternoon'
+      return "afternoon";
     } else {
-      return 'night'
+      return "night";
     }
   }
-})
+});
 
-function i18n (lang) {
-  moment.locale(`${lang.toLowerCase()}-custom`)
+function i18n(lang) {
+  moment.locale(`${lang.toLowerCase()}-custom`);
 }
 
-Object.defineProperty(moment, 'i18n', {
-  get: function () {
-    return i18n
+Object.defineProperty(moment, "i18n", {
+  get: function() {
+    return i18n;
   }
-})
+});
 
-module.exports = moment
+function durationFormatHMS(
+  seconds,
+  {
+    hAlwaysShow = false,
+    hZeroPadWidth = 1,
+    mZeroPadWidth = 1,
+    sZeroPadWidth = 2,
+    mNoZeroPad = false
+  } = {}
+) {
+  const milliseconds = seconds * 1000;
+
+  let h = "";
+
+  const hours = parseInt(moment.duration(milliseconds).asHours());
+
+  if (hAlwaysShow || hours) {
+    h = `${String(hours).padStart(hZeroPadWidth, "0")}:`;
+    if (!mNoZeroPad) {
+      mZeroPadWidth = 2;
+    }
+  }
+
+  return `${h +
+    String(moment.duration(milliseconds).minutes()).padStart(
+      mZeroPadWidth,
+      "0"
+    )}:${String(moment.duration(milliseconds).seconds()).padStart(
+    sZeroPadWidth,
+    "0"
+  )}`;
+}
+
+Object.defineProperty(moment, "durationFormatHMS", {
+  get: function() {
+    return durationFormatHMS;
+  }
+});
+
+module.exports = moment;
