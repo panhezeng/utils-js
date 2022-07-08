2b80f13622f26dea10108814e6fa13c6eb8a7045
panhezeng <panhezeng@gmail.com>
2022-07-08 16:27:34 +0800

    feat: cleanHtml转换转义符

--- packages/lib/dist/cookies.d.ts
+++ packages/lib/dist/cookies.d.ts
@@ -1,7 +1,7 @@
 declare const _default: {
     getItem(sKey: string): string | null;
-    setItem(sKey: string, sValue: string, vEnd?: string | number | Date | undefined, sPath?: string | undefined, sDomain?: string | undefined, bSecure?: string | undefined): boolean;
-    removeItem(sKey: string, sPath?: string | undefined, sDomain?: string | undefined): boolean;
+    setItem(sKey: string, sValue: string, vEnd?: number | string | Date, sPath?: string, sDomain?: string, bSecure?: string): boolean;
+    removeItem(sKey: string, sPath?: string, sDomain?: string): boolean;
     hasItem(sKey: string): boolean;
     keys(): string[];
 };
