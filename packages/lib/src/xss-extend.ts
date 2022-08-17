6038e2d5962b30092a0aa4da4358a5daf38cc8a3
panhezeng <panhezeng@gmail.com>
2022-08-17 15:28:53 +0800

    feat: cleanHtml转换转义符

--- packages/lib/src/xss-extend.ts
+++ packages/lib/src/xss-extend.ts
@@ -1,16 +1,17 @@
-import xssType from 'xss';
+import xssType, { IFilterXSSOptions } from 'xss';
 
 export default function (xss: typeof xssType) {
   /**
    * 过滤所有HTML，得到纯文本
-   * @param source
+   * @param html
    * @returns {string}
    */
-  function cleanHtml(source: string) {
-    return xss(source, {
+  function cleanHtml(html: string, options?: IFilterXSSOptions) {
+    return xss(html, {
       whiteList: {}, // 白名单为空，表示过滤所有标签
       stripIgnoreTag: true, // 过滤所有非白名单标签的HTML
       stripIgnoreTagBody: ['script'], // script标签较特殊，需要过滤标签中间的内容
+      ...options,
     })
       .trim()
       .replace(/&nbsp;/g, ' ')
